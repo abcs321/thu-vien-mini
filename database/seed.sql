3799bb8204d8a3195f4da301edaf92c28d4f1148
Thanh2305 <thanh050999@gmail.com>
2026-08-24 17:42:15 +0700

    Create seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+USE thu_vien_mini;
+
+INSERT INTO doc_gia
+(ho_ten, mssv, lop, so_dien_thoai, ten_dang_nhap, mat_khau)
+VALUES
+('Nguyễn Văn A', '224001801', 'CNTT1', '0912345678', 'nguyenvana', '123456'),
+('Trần Thị B', '224001802', 'CNTT1', '0987654321', 'tranthib', '123456'),
+('Lê Văn C', '224001803', 'CNTT2', '0901234567', 'levanc', '123456');
